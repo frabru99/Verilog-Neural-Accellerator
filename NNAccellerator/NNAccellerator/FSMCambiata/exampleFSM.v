1676e82178d024595df89b9561c141dbb2650d7e
frabru99 <128138203+frabru99@users.noreply.github.com>
2023-12-07 15:05:28 +0100

    Add files via upload

---
+++ NNAccellerator/NNAccellerator/FSMCambiata/exampleFSM.v
@@ -0,0 +1,125 @@
+module FSM (
+    input clk,                // Clock input
+    input reset,              // Reset input
+    input valid,              // Input data valid from feeder
+    input ready_out,          // Input data ready from consumer
+    
+    output reg valid_out,     // Output data valid to consumer
+    output reg ready,         // Output data ready to accelerator
+    output reg enable_reg1,   // Enable signal for Reg 1
+    output reg enable_reg2,   // Enable signal for Reg 2
+    output reg enable_reg3  // Enable signal for Reg 3
+);
+
+    // Define FSM states
+    parameter [2:0] EMPTY_REGISTERS = 3'b000;
+    parameter [2:0] FIRST_PIPE_STAGE_FULL = 3'b001;
+    parameter [2:0] SECOND_PIPE_STAGE_FULL = 3'b010;
+  	parameter [2:0] PIPE_FULL_RESULT_UNREAD = 3'b011;
+    parameter [2:0] PIPE_FULL_RESULT_READ = 3'b100;
+
+    // Define FSM signals
+    reg [2:0] state, next_state;
+    reg [1:0] reg_count; // Count of valid data registers
+
+    // FSM logic
+    always @(posedge clk or posedge reset) begin
+      
+        if (reset) begin
+            state <= EMPTY_REGISTERS;
+            reg_count <= 0;
+            ready <= 1'b1;
+            valid_out <= 1'b0;
+            enable_reg1 <= 1'b0;
+            enable_reg2 <= 1'b0;
+            enable_reg3 <= 1'b0;
+          
+        end else begin
+          
+          state <= next_state;
+          
+        if (state == EMPTY_REGISTERS || (state == FIRST_PIPE_STAGE_FULL && !valid_out) ||
+            state == SECOND_PIPE_STAGE_FULL || state == PIPE_FULL_RESULT_READ) begin
+            ready <= 1'b1;
+    
+        end else begin
+            ready <= 1'b0;
+        end
+
+          if (/*state == PIPE_FULL_RESULT_READ ||*/ state == SECOND_PIPE_STAGE_FULL /*|| state == PIPE_FULL_RESULT_UNREAD*/) begin
+            valid_out <= 1'b1;
+          end else if (state==EMPTY_REGISTERS || state==FIRST_PIPE_STAGE_FULL) begin
+            valid_out <= 1'b0;
+        end
+          
+        end
+    end
+  
+  
+
+    // Next-state logic
+    always @(posedge clk or posedge reset) begin
+        if (reset) begin
+            next_state <= EMPTY_REGISTERS;
+          
+        end else begin
+            case (state)
+              
+                EMPTY_REGISTERS: begin
+                    if (valid && ready) begin
+                        next_state <= FIRST_PIPE_STAGE_FULL;
+                        enable_reg1 <= 1'b1;
+                        enable_reg2 <= 1'b1;
+                        enable_reg3 <= 1'b0;
+                    end else begin
+                        next_state <= EMPTY_REGISTERS;
+                    end
+                end
+
+                FIRST_PIPE_STAGE_FULL: begin
+                  //if (valid && ready) begin
+                  	    next_state <= SECOND_PIPE_STAGE_FULL;
+                        enable_reg1 <= 1'b0;
+                        enable_reg2 <= 1'b0;
+                        enable_reg3 <= 1'b1;
+                    /*end else begin
+                        next_state <= FIRST_PIPE_STAGE_FULL;*/
+                   // end
+                end
+
+                SECOND_PIPE_STAGE_FULL: begin
+                  if (ready_out) begin
+                        next_state <= PIPE_FULL_RESULT_UNREAD;
+                        enable_reg1 <= 1'b0;
+                        enable_reg2 <= 1'b0;
+                        enable_reg3 <= 1'b1;
+                    end else begin
+                        next_state <= SECOND_PIPE_STAGE_FULL;
+                    end
+                end
+
+                PIPE_FULL_RESULT_UNREAD: begin
+                    if (ready_out && valid_out) begin
+                        next_state <= PIPE_FULL_RESULT_READ;
+                        enable_reg1 <= 1'b0;
+                        enable_reg2 <= 1'b0;
+                        enable_reg3 <= 1'b1;
+                    end else begin
+                        next_state <= PIPE_FULL_RESULT_UNREAD;
+                    end
+                end
+
+                PIPE_FULL_RESULT_READ: begin
+                  if (valid) begin
+                        next_state <= FIRST_PIPE_STAGE_FULL;
+                        enable_reg1 <= 1'b1;
+                        enable_reg2 <= 1'b1;
+                        enable_reg3 <= 1'b0;
+                    end else begin
+                        next_state <=  PIPE_FULL_RESULT_READ;
+                    end
+                end
+            endcase
+        end
+    end
+endmodule
---
+++ NNAccellerator/NNAccellerator/acc_pipe.v
@@ -0,0 +1,60 @@
+`include "register.v"
+`include "neuron.v"
+`include "exampleFSM.v"
+
+module acc_pipe(X1, X2, X3, X4, Y, ready, valid, ready_out, valid_out, clk, arst);
+	input signed [7:0] X1, X2, X3, X4;
+	output signed [7:0] Y;
+	input valid, ready_out;
+	output ready, valid_out;
+	input clk, arst;
+
+
+	reg signed [7:0] S1, S2;
+	wire signed [7:0] Y1, Y2, Y3;
+
+	reg enable1, enable2, enable3;
+
+	
+	//parameters for N1
+	parameter [7:0] n1_w1 = -8'd115;
+	parameter [7:0] n1_w2 = 8'd1;
+	parameter [7:0] n1_w3 = -8'd105;
+	parameter [7:0] n1_w4 = 8'd16;
+	parameter [15:0] n1_bias = 16'd12571;
+	parameter [11:0] n1_xmin = -12'd127;
+	parameter [11:0] n1_xmax = 12'd127;
+	//parameters for N2
+
+
+	parameter [7:0] n2_w1 = 8'd103;
+	parameter [7:0] n2_w2 = -8'd22;
+	parameter [7:0] n2_w3 = 8'd32;
+	parameter [7:0] n2_w4 = -8'd56;
+	parameter [15:0] n2_bias = -16'd8139;
+	parameter [11:0] n2_xmin = -12'd127;
+	parameter [11:0] n2_xmax = 12'd127;
+
+
+	//parameters for N3
+	parameter [7:0] n3_w1 = 8'd75;
+	parameter [7:0] n3_w2 = -8'd85;
+	parameter [7:0] n3_w3 = -8'd38;
+	parameter [7:0] n3_w4 = 8'd92;
+	parameter [15:0] n3_bias = 16'd10182;
+	parameter [11:0] n3_xmin = -12'd127;
+	parameter [11:0] n3_xmax = 12'd127;
+
+	/*Add your code to complete the description*/
+
+	exampleFSM fsm(.clk(clk), .reset(arst), .valid(valid), .ready_out(ready_out), .valid_out(valid_out), .ready(ready), .enable_reg1(enable1), .enable_reg2(enable2), .enable_reg3(enable3));
+	Register reg1(.clk(clk), .reset(arst), .data_in(Y1), .data_out(S1), .enable_reg1(enable1));
+	Register reg2(.clk(clk), .reset(arst), .data_in(Y2), .data_out(S2), .enable_reg2(enable2)); //aggiungere segnali di enable registri
+	Register reg3(.clk(clk), .reset(arst), .data_in(Y3), .data_out(Y), .enable_reg3(enable3));
+
+	neuron n1(.X1(X1),.X2(X2), .X3(X3), .X4(X4), .W1(n1_w1), .W2(n1_w2), .W3(n1_w3), .W4(n1_w4), .bias(n1_bias), .xmin(n1_xmin), .xmax(n1_xmax), .Y(Y1));
+	neuron n2(.X1(X1),.X2(X2), .X3(X3), .X4(X4), .W1(n2_w1), .W2(n2_w2), .W3(n2_w3), .W4(n2_w4), .bias(n2_bias), .xmin(n2_xmin), .xmax(n2_xmax), .Y(Y2));
+	neuron n3(.X1(S1),.X2(S2), .X3(0), .X4(0), .W1(n3_w1), .W2(n3_w2), .W3(n3_w3), .W4(n3_w4), .bias(n3_bias), .xmin(n3_xmin), .xmax(n3_xmax), .Y(Y3));
+
+	
+endmodule
